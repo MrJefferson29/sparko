0d1fe88f93be8b96e49bda1e57a6f072bcf10fb9
MrJefferson29 <thejefferson29@gmail.com>
2025-01-31 20:20:38 +0100

    first commit

--- backend/Routers/story.js
+++ backend/Routers/story.js
@@ -1,13 +1,15 @@
 const express = require("express")
-const imageupload = require("../Helpers/Libraries/imageUpload");
 
 const { getAccessToRoute } = require("../Middlewares/Authorization/auth");
 const {addStory,getAllStories,detailStory,likeStory, editStory, deleteStory, editStoryPage } = require("../Controllers/story")
 const { checkStoryExist, checkUserAndStoryExist } = require("../Middlewares/database/databaseErrorhandler");
+const multer = require("multer");
 
 const router = express.Router() ;
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
 
-router.post("/addstory" ,[getAccessToRoute, imageupload.single("image")],addStory)
+router.post("/addstory" ,[getAccessToRoute, upload.single("my_file")],addStory)
 
 
 router.post("/:slug", checkStoryExist, detailStory)
@@ -16,7 +18,7 @@ router.post("/:slug/like",[getAccessToRoute,checkStoryExist] ,likeStory)
 
 router.get("/editStory/:slug",[getAccessToRoute,checkStoryExist,checkUserAndStoryExist] , editStoryPage)
 
-router.put("/:slug/edit",[getAccessToRoute,checkStoryExist,checkUserAndStoryExist, imageupload.single("image")] ,editStory)
+router.put("/:slug/edit",[getAccessToRoute,checkStoryExist,checkUserAndStoryExist] ,editStory)
 
 router.delete("/:slug/delete",[getAccessToRoute,checkStoryExist,checkUserAndStoryExist] ,deleteStory)
 
--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,11 @@ const express = require("express")
 const dotenv = require("dotenv")
 const cors = require("cors")
 const path = require("path")
+const cloudinary = require("cloudinary").v2;
+const multer = require("multer");
 
 const IndexRoute = require("./Routers/index")
+const storyRouter = require("./Routers/story");
 const connectDatabase = require("./Helpers/database/connectDatabase")
 const customErrorHandler = require("./Middlewares/Errors/customErrorHandler")
 
@@ -11,18 +14,27 @@ dotenv.config({ path: './.env' });
 
 connectDatabase()
 
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET,
+});
+
 const app = express() ;
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
 
 app.use(express.json())
 app.use(cors())
 
 app.use("/",IndexRoute)
+app.use("/story", storyRouter);
 
 app.use(customErrorHandler)
 
 const PORT = process.env.PORT || 5000 ;
 
-app.use(express.static(path.join(__dirname , "public") ))
+app.use(express.static(path.join(__dirname, "public")));
 
 const server = app.listen(PORT,()=>{
 
