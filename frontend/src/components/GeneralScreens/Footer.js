c92dc6cdee30f8f8c10613c75349547b75b77930
MrJefferson29 <100526664+MrJefferson29@users.noreply.github.com>
2026-02-08 16:08:26 +0100

    Update Footer.js

--- frontend/src/components/GeneralScreens/Footer.js
+++ frontend/src/components/GeneralScreens/Footer.js
@@ -10,7 +10,7 @@ function Footer() {
         <Row>
           <Col md={4}>
             <FooterSection>
-              <FooterLogo>French Bulldog Kennel US</FooterLogo>
+              <FooterLogo>French Bulldog Kennel UK</FooterLogo>
               <FooterDescription>
                 Quality bred French Bulldogs pupps, ready to find a place in your home, and in your heart
                 contact us, or visit our store to know more about us.
@@ -47,7 +47,7 @@ function Footer() {
         </Row>
       </Container>
       <FooterBottom>
-        <p>&copy; 2024 French Bulldog Kennel US</p>
+        <p>&copy; 2024 French Bulldog Kennel UK</p>
       </FooterBottom>
     </FooterContainer>
   );
